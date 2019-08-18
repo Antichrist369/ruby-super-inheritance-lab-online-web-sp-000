4d7ef6ef3ed1675210b8bb59f9c1777159537342
Anthony Molina <antonio-molina@att.net>
2019-08-18 04:05:00 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -1,6 +1,6 @@
 class Chatty_student < Student 
   def hello 
     super 
-    hello puts "How are you doing today? I'm okay, but I'm kind of tired."
+    hello puts "How are you doing today? I'm okay, but I'm kind of tired. Did you watch the Walking Dead last night? You didn't?! oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
   end 
 end 
